ef7570fb3b75526f1c60864bf51853aafde08ae3
Rick Barrette <rickbarrette@gmail.com>
2026-03-12 11:23:07 -0400

    Refactored to use QboBaseModel & ServiceBase from redmine_qbo

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,51 +8,18 @@
 #
 #THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-class Item < ApplicationRecord
+class Item < QboBaseModel
   belongs_to :issue
 
   validates_presence_of :id, :description
   validates :unit_price, numericality: { greater_than_or_equal_to: 0 }
   self.primary_key = :id
 
-  # Returns the details of the item. If the details have already been fetched, it returns the cached version. Otherwise, it fetches the details from QuickBooks Online and caches them for future use. This method is used to access the item's information in a way that minimizes unnecessary API calls to QBO, improving performance and reducing latency.
-  def details
-    @details ||= begin
-      xml = Rails.cache.fetch(details_cache_key, expires_in: 10.minutes) do
-        fetch_details.to_xml_ns
-      end
-      Quickbooks::Model::Item.from_xml(xml)
-    end
-  end
-
-   # Generates a unique cache key for storing this customer's QBO details.
-  def details_cache_key
-    "item:#{id}:qbo_details:#{updated_at.to_i}"
-  end
-
-
   # Updates Both local & remote DB description
   def description=(s)
     details
     @details.description = s
     super
-  end
-
-  # Returns the last sync time formatted for display. If no sync has occurred, returns a default message.
-  def self.last_sync
-    return I18n.t(:label_qbo_never_synced) unless maximum(:updated_at)
-    format_time(maximum(:updated_at))
-  end
-
-  # Magic Method
-  # Maps Get/Set methods to QBO item object
-  def method_missing(method_name, *args, &block)
-    if Quickbooks::Model::Item.method_defined?(method_name)
-      details
-      @details.public_send(method_name, *args, &block)
-    else
-      super
-    end
   end
 
   # Updates Both local & remote DB name 
@@ -69,47 +36,11 @@ class Item < ApplicationRecord
     super
   end
 
-  # Sync all items, typically triggered by a scheduled task or manual sync request
-  def self.sync
-    ItemSyncJob.perform_later(full_sync: true)
-  end
-
-  # Sync a single items by ID, typically triggered by a webhook notification or manual sync request
-  def self.sync_by_id(id)
-    ItemSyncJob.perform_later(id: id)
-  end
-
-  # Push the updates
-  def save_with_push
-    log "Starting push for item ##{self.id}..."
-    qbo = QboConnectionService.current!
-    ItemService.new(qbo: qbo, item: self).push()
-    Rails.cache.delete(details_cache_key)
-    save_without_push
-  end
-
-   alias_method :save_without_push, :save
-   alias_method :save, :save_with_push
-
    # Updates Both local & remote DB price
   def unit_price=(s)
     details
     @details.unit_price = s
     super
   end
-
-  private
-  
-  def log(msg)
-    Rails.logger.info "[Item] #{msg}"
-  end
-
-  # Fetches the item's details from QuickBooks Online. 
-  def fetch_details
-    log "Fetching details for item ##{id} from QBO..."
-    qbo = QboConnectionService.current!
-    ItemService.new(qbo: qbo, item: self).pull()
-  end
-
 
 end
--- app/services/item_service.rb
+++ app/services/item_service.rb
@@ -8,17 +8,11 @@
 #
 #THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-class ItemService
+class ItemService < ServiceBase
 
-  # Initializes the service with a QBO client and an optional item record. The QBO client is used to communicate with QuickBooks Online, while the item record contains the data that needs to be pushed to QBO. If no item is provided, the service will not perform any operations.
-  def initialize(qbo:, item: nil)
-    raise "No QBO configuration found" unless qbo
-    raise "Item record is required for push operation" unless item
-    @qbo = qbo
-    @item = item
-  end
+  private
 
-  def build_qbo_item
+  def build_qbo_remote
     log "Building new QBO Item"
     account = default_income_account
     log "Account: #{account.id} - #{account.name}"
@@ -42,52 +36,6 @@ class ItemService
       )
       service.query("SELECT * FROM Account WHERE AccountType='Income' AND Name LIKE '%Sales%'").first
     end
-  end
-
-  # Pulls the Item data from QuickBooks Online. 
-  def pull
-    return Quickbooks::Model::Item.new unless @item.present?
-    return build_qbo_item unless @item.id
-    log "Fetching details for item ##{@item.id} from QBO..."
-    qbo = QboConnectionService.current!
-    qbo.perform_authenticated_request do |access_token|
-      service = Quickbooks::Service::Item.new(
-        company_id: qbo.realm_id,
-        access_token: access_token
-      )
-      service.fetch_by_id(@item.id)
-    end
-  rescue => e
-    log "Fetch failed for #{@item.id}: #{e.message}"
-    build_qbo_item
-  end
-
-   # Pushes the Item data to QuickBooks Online. This method handles the communication with QBO, including authentication and error handling. It uses the QBO client to send the item data and logs the process for monitoring and debugging purposes. If the push is successful, it returns the item record; otherwise, it logs the error and returns false.
-  def push
-    log "Pushing item ##{@item.id} to QBO..."
-
-    item = @qbo.perform_authenticated_request do |access_token|
-      service = Quickbooks::Service::Item.new( 
-        company_id: @qbo.realm_id, 
-        access_token: access_token 
-      )
-      if @item.id.present?
-        service.update(@item.details)
-      else
-        service.create(@item.details)
-      end
-    end
-
-    @item.id = item.id unless @item.persisted?
-    log "Push for item ##{@item.id} completed."
-    return @item
-  end
-
-  private 
-
-  # Log messages with the entity type for better traceability
-  def log(msg)
-    Rails.logger.info "[ItemService] #{msg}"
   end
 
 end
